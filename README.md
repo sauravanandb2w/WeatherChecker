9ee7136b74d3812ef709af37657a88184468b73e
saurav anand <anandsaurav100@gmail.com>
2019-05-15 05:24:24 +0530

    Adding python files and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+##################A Weather Checker tool###############
+> OpenWeathermap API is used.
+> it will provide information about temperature of any place in celcius.
+> it will also provide some extended weather descriptions.
+> user interactive GUI allowing user to search place of his/her Choice.
+> under development 
---
+++ views.py
@@ -0,0 +1,35 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=9978bb23d5e283eb2594e0eee787f6aa'
+    #city = 'bela'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    #print(weather_data)
+    context = {'weather_data': weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
